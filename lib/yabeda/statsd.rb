1de2e2ee2b17d51f96afccdb8d5634cdc575c669
Alexander Susikov <susikov.alexander@gmail.com>
2019-11-12 00:55:25 +0300

    Added dsl methods

--- lib/yabeda/statsd.rb
+++ lib/yabeda/statsd.rb
@@ -1,8 +1,42 @@
+# frozen_string_literal: true
+
 require "yabeda/statsd/version"
 
 module Yabeda
+  # Namespace for Statsd adapter
   module Statsd
-    class Error < StandardError; end
-    # Your code goes here...
+    class << self
+      def configure(&block)
+        class_exec(&block)
+      end
+
+      def config
+        Yabeda::Statsd::Config.config
+      end
+
+      def global_label(label_name, value:)
+        global_labels[label_name] = value
+      end
+
+      def global_labels
+        @global_labels ||= Concurrent::Hash.new
+      end
+
+      def start(logger: nil)
+        adapter = Yabeda::Statsd::Adapter.new(logger: logger)
+        Yabeda.register_adapter(:statsd, adapter)
+        adapter
+      end
+
+      # Start collection metrics from Yabeda collectors
+      def start_exporter
+        Thread.new do
+          loop do
+            Yabeda.collectors.each(&:call)
+            sleep(config.collect_interval)
+          end
+        end
+      end
+    end
   end
 end
