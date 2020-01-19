f01fc3a04ce9e86c8f2cda2bac9031f4d724da66
Alexander Susikov <susikov.alexander@gmail.com>
2020-01-19 21:32:43 +0300

    Removed unused specs

--- spec/yabeda/statsd_spec.rb
+++ spec/yabeda/statsd_spec.rb
@@ -1,9 +1,7 @@
+# frozen_string_literal: true
+
 RSpec.describe Yabeda::Statsd do
   it "has a version number" do
     expect(Yabeda::Statsd::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
   end
 end
